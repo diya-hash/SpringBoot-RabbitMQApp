0e832f7cdd4780cc819fd8f12f4707df4c247250
Diya Salam <sdiya.salam.github@gmail.com>
2025-02-16 17:54:59 -0500

    add

---
+++ RabbitMQApp/src/main/java/com/rabbitmq/example/RabbitMQApp/consumer/RabbitMQDEConsumerAC.java
@@ -0,0 +1,29 @@
+package com.rabbitmq.example.RabbitMQApp.consumer;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Service;
+
+import com.rabbitmq.client.Channel;
+
+@Service
+public class RabbitMQDEConsumerAC {
+	@RabbitListener(queues = "AC", ackMode = "MANUAL") 
+	 public void receiveMessage(String message, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long tag) {
+	    try {
+	        System.out.println("Received messagein AC: " + message);
+	        // ✅ Manually acknowledge the message
+	        channel.basicAck(tag, false);
+	        System.out.println("Message acknowledged!");
+	    } catch (Exception e) {
+	        try {
+	            // ❌ Reject message if processing fails (Requeue = false means it will be discarded)
+	            channel.basicNack(tag, false, false);
+	            System.out.println("Message rejected!");
+	        } catch (Exception ex) {
+	            ex.printStackTrace();
+	        }
+	    }
+	}
+}
